23483012d09fd55f673751d287f2bd30e8910c94
Soslan <sparksoslan@gmail.com>
2020-05-20 21:42:01 +0100

    Add User component and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-function App() {
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+// import './App.css';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Users />
+        </Route>
+        <Route exact path="/places/new">
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/user/components/UserItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './UserItem.css';
+
+const UserItem = () => {
+  return <h1>UserItem Component</h1>;
+};
+
+export default UserItem;

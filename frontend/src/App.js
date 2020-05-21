f83603489514725f16019bd1000302246c11810f
Soslan <sparksoslan@gmail.com>
2020-05-21 20:07:08 +0100

    Add navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 // import './App.css';
@@ -8,15 +9,18 @@ import NewPlace from './places/pages/NewPlace';
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Users />
-        </Route>
-        <Route exact path="/places/new">
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route exact path="/places/new">
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };

f12f1f8633b0ab1993bebce62984b17be9fb1128
Soslan <sparksoslan@gmail.com>
2020-05-25 19:21:37 +0100

    Add error 404 handling for unsupported routes

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,18 @@ const bodyParser = require('body-parser');
 const PORT = 5000;
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-errors');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
